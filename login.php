be9270fc1bd5f1d92cbf6888f483d68356a0ae9d
Mirko Piras <mirkopiras01@gmail.com>
2021-05-02 23:10:12 +0200

    init

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <center>
+    <h2>Login</h2>
+    <a href="index.html">Torna alle homepage</a>
+    <form>
+        <legend for="email">Email</legend>
+        <input name="email" type="email">
+        <legend for="pwd">Password</legend>
+        <input name="pwd" type="password">
+        <br><br>
+        <a href="signup.html">Registrati</a>
+    </form>
+</center>
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <center>
+    <h2>Login</h2>
+    <form style="margin: 50px">
+        <a href="index.html">Torna alle homepage</a><br>
+        <label for="email">Email</label>
+        <input name="email"><br>
+        <label for="pwd">Password</label>
+        <input name="pwd" type="password"><br>
+        <input type="submit" value="Registrati">
+    </form>
+    <a href="login.html">Login</a>
+</center>
+
+<?php
+    $email = $pwd = "";
+
+?>
+
+
+
+</body>
+
+</html>
