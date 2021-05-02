81db3ae61d9aaf7c547ee983583f5c6de0051054
Mirko Piras <mirkopiras01@gmail.com>
2021-05-03 00:04:38 +0200

    signup working

--- php/signup.php
+++ php/signup.php
@@ -9,12 +9,12 @@
 <body>
     <center>
     <h2>Registrati</h2>
-    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="margin: 50px">
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <a href="index.php">Torna alle homepage</a><br>
         <label for="email">Email</label>
-        <input name="email"><br>
+        <input name="email" placeholder="Email" required><br>
         <label for="password">Password</label>
-        <input name="password" type="password"><br>
+        <input name="password" type="password" placeholder="Password" required><br>
         <input type="submit" value="Registrati">
     </form>
     <a href="login.php">Login</a>
@@ -32,14 +32,17 @@
         $email = $_POST['email'];
         $password = $_POST['password'];
 
+        // controllo valori inseriti 
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid format and please re-enter valid email"; 
+        }
+
         // query
         $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password');";
 
         if ($pdo->query($sql) === TRUE) {
         echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        } 
 
     }
 
