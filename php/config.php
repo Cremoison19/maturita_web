2d410e93e7e4ff9b62d004199d142ffc87f85761
Mirko Piras <mirkopiras01@gmail.com>
2021-05-03 09:27:15 +0200

    validation and password being crypted

--- php/config.php
+++ php/config.php
@@ -5,12 +5,33 @@
     define('DB_PASSWORD', '');
     define('DB_NAME', 'agen_colloc');
 
+    $PWD_MIN_LENGTH = 6; 
+    // lunghezza minima delle password, impostata qua come variabile globale
+
+    // errori globali
+    $dpasswordErr = "Password non valida. Inserisci una password di lunghezza maggiore di 6 caratteri e <br> con almeno un numero.";
+    $demailErr = "Email non valida. Assicurati che non ci siano errori di battitura.";
+
+
+
     try{
         $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
         // Set the PDO error mode to exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
         die("ERROR: Could not connect. " . $e->getMessage());
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    function cryptp($data) {
+        return md5(md5($data)."50blessings");
+        // dopo aver criptato la stringa una volta le aggiungiamo una salt, per poi criptare il tutto un'altra volta
     }
 
 
--- php/signup.php
+++ php/signup.php
@@ -7,20 +7,10 @@
 </head>
 
 <body>
-    <center>
-    <h2>Registrati</h2>
-    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <a href="index.php">Torna alle homepage</a><br>
-        <label for="email">Email</label>
-        <input name="email" placeholder="Email" required><br>
-        <label for="password">Password</label>
-        <input name="password" type="password" placeholder="Password" required><br>
-        <input type="submit" value="Registrati">
-    </form>
-    <a href="login.php">Login</a>
-</center>
 
 <?php
+
+    $emailErr = $passwordErr = "";
     
     // file con impostazioni del database
     require_once "config.php";
@@ -29,28 +19,67 @@
     $email = $password = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $validate = true;
+
         // ottenimento valori dal form
         $email = $_POST['email'];
         $password = $_POST['password'];
 
         // controllo valori inseriti 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Invalid format and please re-enter valid email"; 
+
+        if (empty($_POST["email"])) {
+            $emailErr = "L'email è obbligatoria";
+        } else {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = $demailErr;
+                $validate = false;
+            }
         }
 
-        // query
-        $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password');";
+        if (empty($_POST["password"])) {
+            $password = "La password è obbligatoria";
+        } else {
+            if(strlen($password)<=$PWD_MIN_LENGTH){
+                $passwordErr = $dpasswordErr;
+                $validate = false;
+            }
+        }
 
-        if ($pdo->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } 
+        // query (solo se tutti i campi sono stati validati)
+        if($validate){
+
+            // se la validazione è avvenuta correttamente e quindi la password è corretta, possiamo criptarla prima di inserirla nel database
+            $password_c = cryptp($password);
+
+            $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password_c');";
+
+            if ($pdo->query($sql) === TRUE) {
+            echo "New record created successfully";
+            } 
+        }
+        else{
+            echo "La validazione ha riscontrato degli errori";
+        }
 
     }
 
 ?>
 
-
-
+    <center>
+    <h2>Registrati</h2>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <a href="index.php">Torna alle homepage</a><br>
+        <label for="email">Email</label>
+        <input name="email" placeholder="Email" required maxlenght="320">
+        <span><?php echo $emailErr;?>* </span><br>
+        <label for="password">Password</label>
+        <input name="password" type="password" placeholder="Password" required maxlenght="255">
+        <span><?php echo $passwordErr;?>* </span><br>
+        <input type="submit" value="Registrati">
+    </form>
+    <a href="login.php">Login</a>
+</center>
 </body>
 
 </html>
