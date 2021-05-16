87797edad338655635415cd96f0bea5ea4c6b87b
Mirko Piras <mirkopiras.01@gmail.com>
2021-05-16 13:43:03 +0200

    reject users bug fix

--- php/admin/reject.php
+++ php/admin/reject.php
@@ -1,12 +1,14 @@
 <?php
-session_start();
-if ($_SESSION['userID'] != "admin") header("Location: ../index.php");
-$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$id = explode("=", parse_url($url, PHP_URL_QUERY))[1];
+    session_start();
+    if ($_SESSION['userID'] != "admin") header("Location: ../index.php");
+    $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $id = explode("=", parse_url($url, PHP_URL_QUERY))[1];
 
-require_once "../config.php";
+    require_once "../config.php";
 // just delete request
-$id = $request_user["id"];
-$pdo->query("DELETE FROM requests WHERE id = '$id';");
-//echo "<a href='home.php'>home</a>";
-header("Location: accept_users.php");
+    $request_user = $pdo->query("SELECT * FROM requests WHERE id = '$id';")->fetch();
+    $id = $request_user["id"];
+    $pdo->query("DELETE FROM requests WHERE id = '$id';");
+    //echo "<a href='home.php'>home</a>";
+    header("Location: accept_users.php");
+?>
